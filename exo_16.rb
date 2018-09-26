af171d4fe543e2ae712f192d64a9010d06419e9a
Zybou <hoang_christine@yahoo.fr>
2018-09-26 00:28:13 +0000

    exo 16 et 21 done youpiiii

--- exo_16.rb
+++ exo_16.rb
@@ -2,9 +2,12 @@
 puts "C est quoi ton annee de naissance?"
 birthyear = gets.chomp
 actualyear = 2017
-birthyearvar = 2016
+birthyearvar = 2017
 
+until birthyearvar < birthyear.to_i
 numbyear1 = actualyear - birthyearvar
 numbyear2 = birthyearvar- birthyear.to_i
 
-puts " Il y a " + numbyear1.to_s + " ans tu avais " + numbyear2.to_s + " ans."
+puts " Il y a " + numbyear2.to_s + " ans tu avais " + numbyear1.to_s + " ans."
+birthyearvar =birthyearvar-1
+end
